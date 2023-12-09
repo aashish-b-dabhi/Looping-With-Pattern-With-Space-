f6bc0766561a1ea14d86fd487d762bc04baa5fb4
DABHI AASHISH BHARATBHAI <150463240+aashish-b-dabhi@users.noreply.github.com>
2023-12-09 21:09:57 +0530

    Add files via upload

---
+++ Pattern - 17.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j,k;
+	
+	for(i=5 ; i>=1 ; i--)
+	{
+		
+		for(k=i ; k>1 ; k--)
+		{
+			printf(" ");
+		}
+		for(j=5 ; j>=i ; j--)
+		{
+			if(j % 2 == 1)
+			{
+				printf("1");
+			}
+			else
+			{
+				printf("0");
+			}
+		}
+		
+		printf("\n");
+		
+	}
+	
+}
